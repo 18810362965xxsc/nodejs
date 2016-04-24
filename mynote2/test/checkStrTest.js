efdeb8d358bc616f058e46812a7969575eed1263
18810352965xxsc <471473870@qq.com>
2016-04-24 10:54:33 +0800

    6

--- mynote2/test/checkStrTest.js
+++ mynote2/test/checkStrTest.js
@@ -1,10 +1,10 @@
 var checkStr=require('../util/checkStr')
 
-err = checkStr.registerCheck('章伟','','');
+err = checkStr.registerCheck('陈静','','');
 console.log(err);
-err = checkStr.registerCheck('zhangwei_','Zhangwei001','Zhangwei001');
+err = checkStr.registerCheck('chenjing_','Chenjingi001','Chenjing001');
 console.log(err);
-err = checkStr.registerCheck('zhangwei','Zhangwei001','');
+err = checkStr.registerCheck('chenjing','Chenjing001','');
 console.log(err);
 /*err = checkStr.registerCheck('','','');
 console.log(err);
